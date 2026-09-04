4c13f486d5a94180363f05525028491d0a89aae8
Aravindagouda <aravindsomanagoudra0@gmail.com>
2026-09-04 13:25:01 +0530

    initial spring boot boilerplate

---
+++ src/test/java/com/cinepulse/CinepulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinepulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
